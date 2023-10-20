d32d495d685921dd63bef543d8290492f44fd60a
Mukeswar Shah <mukeswar.github@gmail.com>
2023-10-20 12:18:26 +0545

    Installed react and all and created route

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter,Routes,Route } from "react-router-dom"
+import { Profile } from "./pages/Profile"
+import { About } from "./pages/About"
+import { SignUp } from "./pages/SignUp"
+import { SignIn } from "./pages/SignIn"
+import { Home } from "./pages/Home"
+const App = () => {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/sign-in" element={<SignIn/>}/>
+      <Route path="/sign-up" element={<SignUp/>}/>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
